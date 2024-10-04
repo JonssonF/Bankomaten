c83ff7ae39fc283450881f7b0239db96bb9f154f
Fredrik Jonsson <Fredrikandrejonson@hotmail.com>
2024-10-04 19:10:39 +0200

    Just started to make a repository so i can work on both my computers.

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             //Fredrik Jonsson SUT_24
-
+            
             Console.WriteLine("Välkommen till bankomaten:\n\n" +
                 "Var god logga in genom att skriva ditt användarnamn och pinkod.\n\n");
             Console.Write("Användarnamn:");
