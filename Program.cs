8e2c4590f8d7626c411ad951acee68fc837185ae
Fredrik Jonsson <Fredrikandrejonson@hotmail.com>
2024-10-04 19:08:52 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Bankomaten
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Fredrik Jonsson SUT_24
+
+            Console.WriteLine("Välkommen till bankomaten:\n\n" +
+                "Var god logga in genom att skriva ditt användarnamn och pinkod.\n\n");
+            Console.Write("Användarnamn:");
+            string userName = Console.ReadLine();
+            Console.Write("Pinkod:");
+            string userPin = Console.ReadLine();
+
+
+        }
+    }
+}
