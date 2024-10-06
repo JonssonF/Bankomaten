35cdc0f3a65c3481d0905856ac175bac44ab8119
Fredrik Jonsson <Fredrikandrejonson@hotmail.com>
2024-10-06 20:26:07 +0200

    Struggling to get back to the main menu.

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@
                 Console.WriteLine("2. Deposit");
                 Console.WriteLine("3. Withdraw");
                 Console.WriteLine("4. Transfer");
-                Console.WriteLine("5. Exit");
+                Console.WriteLine("5. Logout");
             }
 
             string[,] userName = new string[5, 3] { { "0", "John", "1231" }, { "1", "Jane", "1232" }, { "2", "Jenny", "1233" }, { "3", "James", "1234" }, { "4", "Joel", "1235" } };
@@ -19,57 +19,92 @@
 
 
             bool bankEngine = true;
-            while (bankEngine)
+            while (bankEngine == true)
             {
                 Console.WriteLine("Welcome to the bank:\n\n" +
                     "Please enter your ID and pincode.\n\n");
                 Console.Write("Enter ID:");
                 int verifyUser = Convert.ToInt32(Console.ReadLine());
-
-                Console.Write($"Hi, {userName[verifyUser, 1]}, please enter your pincode:");
-                int verifyPin = Convert.ToInt32(Console.ReadLine());
-                int loginAttempt = 0;
-                bool loginSuccess = false;
-
-                while (loginSuccess == false || loginAttempt <= 3)
+                if (verifyUser >= 5)
                 {
-                    if (verifyPin == userPin[verifyUser])
+                    Console.WriteLine("We don't have so many accounts at the moment.");
+                }
+                else
+                {
+                    Console.Write($"Hi, {userName[verifyUser, 1]}, please enter your pincode:");
+                    int verifyPin = Convert.ToInt32(Console.ReadLine());
+                    int loginAttempt = 0;
+                    bool loginSuccess = false;
+                    bool loginTry;
+                    while (loginTry = true && loginAttempt < 3)
                     {
-                        Console.WriteLine("Pincode accepted.");
-                        Thread.Sleep(2500);
-                        Console.Clear();
-                        loginSuccess = true;
+                        loginAttempt++;
+                        if (verifyPin == userPin[verifyUser])
+                        {
+                            Console.WriteLine("Pincode accepted.");
+                            Thread.Sleep(1500);
+                            Console.Clear();
+                            loginTry = false;
+                            loginSuccess = true;
+                        }
+                        else if (loginAttempt == 3)
+                        {
+                            Console.WriteLine("Access denied.");
+                            Thread.Sleep(2500);
+                            bankEngine = false;
+                            Console.Clear();
+                            return;
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.Write("Wrong pincode, try again: ");
+                            verifyPin = Convert.ToInt32(Console.ReadLine());
+                        }
                         while (loginSuccess == true)
                         {
                             menuOption();
                             string menuChoice = Console.ReadLine();
+                            switch (menuChoice)
+                            {
+                                case "1":
+
+                                    break;
+                                case "2":
+
+                                    break;
+                                case "3":
+
+                                    break;
+                                case "4":
+
+                                    break;
+
+                                case "5":
+                                    Console.Clear();
+                                    Console.WriteLine($"Have a nice day {userName[verifyUser, 1]}!");
+                                    loginSuccess = false;
+                                    break;
+
+                                default:
+                                    Console.WriteLine("Unvalid option, try again.");
+                                    Thread.Sleep(1500);
+                                    Console.Clear();
+                                    break;
+                            }
+                            if (loginSuccess == false)
+                            {
+                                Console.WriteLine("Press enter to return to the main menu.");
+                                Console.ReadLine();
+                                loginAttempt = 0;
+                                verifyPin = 0;
+                                loginTry = false;
+                                break;
+                            }
                         }
                     }
-                    else if (loginAttempt == 3)
-                    {
-                        Console.WriteLine("Access denied.");
-                        Thread.Sleep(2500);
-                        bankEngine = false;
-                        Console.Clear();
-                        return;
-                    }
-                    else
-                    {
-                        loginAttempt++;
-                        Console.Clear();
-                        Console.Write("Wrong pincode, try again: ");
-                        verifyPin = Convert.ToInt32(Console.ReadLine());
-                    }
-
                 }
-                
-
-
             }
         }
-
-        
-
-
     }
 }
